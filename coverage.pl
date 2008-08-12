3fe98af5f1e38e565f9b09aef2ab277bc4265e0f
Cal Henderson <cal@iamcal.com>
2008-08-12 23:38:13 +0000

    new coverage paths for MT box
    git-svn-id: https://svn.iamcal.com/public/perl/XML-Parser-Lite-Tree-XPath/trunk@2435 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- coverage.pl
+++ coverage.pl
@@ -4,4 +4,4 @@ use strict;
 
 print `cover -delete`;
 print `HARNESS_PERL_SWITCHES=-MDevel::Cover make test`;
-print `cover -outputdir /var/www/html/root.flickr.com/cover`;
+print `cover -outputdir /var/www/vhosts/oembed.com/httpdocs/coverage/XML-Parser-Lite-Tree-XPath`;
