32014ae759d1c7cbfdb8cc5d24825683509403aa
Cal Henderson <cal@iamcal.com>
2008-08-13 23:02:13 +0000

    mark uids and namespaces in the tree during the eval phase. namespaces should almost certainly be moved to the Tree module though
    git-svn-id: https://svn.iamcal.com/public/perl/XML-Parser-Lite-Tree-XPath/trunk@2445 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- lib/XML/Parser/Lite/Tree/XPath/Eval.pm
+++ lib/XML/Parser/Lite/Tree/XPath/Eval.pm
@@ -1,6 +1,7 @@
 package XML::Parser::Lite::Tree::XPath::Eval;
 
 use XML::Parser::Lite::Tree::XPath::Token;
+use Data::Dumper;
 use strict;
 
 sub new {
@@ -17,6 +18,16 @@ sub query {
 
 	$self->{root} = XML::Parser::Lite::Tree::XPath::Result->new('nodeset', [$self->{tree}]);
 	$self->{max_order} = $self->mark_orders($self->{tree}, 1, undef);
+
+	$self->{uids} = {};
+	$self->mark_uids($self->{tree});
+
+	unless ($self->{tree}->{ns_done}){
+
+		$self->{ns_stack} = {};
+		$self->mark_namespaces($self->{tree});
+		$self->{tree}->{ns_done} = 1;
+	}
 
 	my $token = $xpath->{tokens}->[0];
 	unless (defined $token){
@@ -64,6 +75,116 @@ sub mark_token {
 
 	for my $child(@{$token->{tokens}}){
 		$self->mark_token($child);
+	}
+}
+
+sub mark_uids {
+	my ($self, $tag) = @_;
+
+	#
+	# mark
+	#
+
+	if ($tag->{type} eq 'tag'){
+
+		$tag->{uid} = '';
+
+		my $id = $tag->{attributes}->{id};
+
+		if (defined $id && length $id){
+			unless (defined $self->{uids}->{$id}){
+
+				$tag->{uid} = $id;
+				$self->{uids}->{$id} = 1;
+			}
+		}
+	}
+
+
+	#
+	# descend
+	#
+
+	if ($tag->{type} eq 'root' || $tag->{type} eq 'tag'){
+
+		for my $child (@{$tag->{children}}){
+
+			$self->mark_uids($child);
+		}
+	}
+}
+
+sub mark_namespaces {
+	my ($self, $obj) = @_;
+
+
+	my @ns_keys;
+
+	#
+	# mark
+	#
+
+	if ($obj->{type} eq 'tag'){
+
+		#
+		# first, add any new NS's to the stack
+		#
+
+		my @keys = keys %{$obj->{attributes}};
+
+		for my $k(@keys){
+
+			if ($k =~ /^xmlns:(.*)$/){
+
+				push @{$self->{ns_stack}->{$1}}, $obj->{attributes}->{$k};
+				push @ns_keys, $1;
+				delete $obj->{attributes}->{$k};
+			}
+
+			if ($k eq 'xmlns'){
+
+				push @{$self->{ns_stack}->{__default__}}, $obj->{attributes}->{$k};
+				push @ns_keys, '__default__';
+				delete $obj->{attributes}->{$k};
+			}
+		}
+
+
+		#
+		# now - does this tag have a NS?
+		#
+
+		if ($obj->{name} =~ /^(.*?):(.*)$/){
+
+			$obj->{local_name} = $2;
+			$obj->{ns_key} = $1;
+			$obj->{ns} = $self->{ns_stack}->{$1}->[-1];
+		}else{
+			$obj->{local_name} = $obj->{name};
+			$obj->{ns} = $self->{ns_stack}->{__default__}->[-1];
+		}
+	}
+
+
+	#
+	# descend
+	#
+
+	if ($obj->{type} eq 'root' || $obj->{type} eq 'tag'){
+
+		for my $child (@{$obj->{children}}){
+
+			$self->mark_namespaces($child);
+		}
+	}
+
+
+	#
+	# pop from stack
+	#
+
+	for my $k (@ns_keys){
+		pop @{$self->{ns_stack}->{$k}};
 	}
 }
 
