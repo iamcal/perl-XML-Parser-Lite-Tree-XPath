54a749d55672a45a001cd4ec7978e04c9d7a4b76
Cal Henderson <cal@iamcal.com>
2008-10-01 04:25:06 +0000

    added docs to the main module. increased coverage (very slightly) of the axis module
    git-svn-id: https://svn.iamcal.com/public/perl/XML-Parser-Lite-Tree-XPath/trunk@2552 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- lib/XML/Parser/Lite/Tree/XPath.pm
+++ lib/XML/Parser/Lite/Tree/XPath.pm
@@ -73,3 +73,56 @@ sub select_nodes {
 
 1;
 
+__END__
+
+=head1 NAME
+
+XML::Parser::Lite::Tree::XPath - XPath access to XML::Parser::Lite::Tree structures
+
+=head1 SYNOPSIS
+
+  use XML::Parser::Lite::Tree;
+  use XML::Parser::Lite::Tree::XPath;
+
+  my $parser = new XML::Parser::Lite::Tree(process_ns => 1);
+  my $tree = $parser->parse($xml);
+
+  my $xpath = new XML::Parser::Lite::Tree::XPath($tree);
+  my $nodes = $xpath->select_nodes('/aaa');
+
+  my $nodes = $xpath->select_nodes('/*/*/parent::*');
+  my $nodes = $xpath->select_nodes('//ccc[ position() = floor(last() div 2 + 0.5) or position() = ceiling(last() div 2 + 0.5) ]');
+
+
+=head1 DESCRIPTION
+
+This pure-Perl implementation of XPath is a complement to XML::Parser::Lite::Tree, a pure-Perl 
+XML tree parser and builder. It aims to implement 100% of the W3C XPath specification.
+
+=head1 METHODS
+
+=over
+
+=item C<new( $tree )>
+
+The constructor returns a new XPath parser for the given tree.
+
+=item C<query( $path )>
+
+Returns a XML::Parser::Lite::Tree::XPath::Result object containing the result of the query.
+
+=item C<select_nodes( $path )>
+
+A convinience function around C<query()> which returns 0 unless the result is a nodeset, else returns the value of the nodeset.
+
+=back
+
+=head1 AUTHORS
+
+Copyright (C) 2004-2008, Cal Henderson <cal@iamcal.com>
+
+=head1 SEE ALSO
+
+L<XML::Parser::Lite::Tree>
+
+=cut
