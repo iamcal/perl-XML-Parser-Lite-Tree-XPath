e704cdbea6ab4ab0e5ad246ff94dc8dfae764ea3
Cal Henderson <cal@iamcal.com>
2008-10-01 04:00:38 +0000

    updated README and Changes to be non-boilerplate
    git-svn-id: https://svn.iamcal.com/public/perl/XML-Parser-Lite-Tree-XPath/trunk@2551 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- lib/XML/Parser/Lite/Tree/XPath.pm
+++ lib/XML/Parser/Lite/Tree/XPath.pm
@@ -7,25 +7,6 @@ use XML::Parser::Lite::Tree::XPath::Eval;
 
 our $VERSION = '0.21';
 
-# v0.10 - tokener finished
-# v0.11 - tree builder started
-# v0.12 - tree builder can tree all zvon examples correctly (t/04_tree2.t)
-# v0.14 - started on the eval engine - zvon examples 1 and 2 eval correctly (t/05_zvon0[12].t)
-# v0.15 - more eval engine work - zvon examples 3,4,5 and some of 6
-# v0.16 - more eval engine work - 6 and some of 7 (ret type coersion)
-# v0.17 - more eval engine work - 7,8,9 (function arg validation)
-# v0.18 - more eval engine work - 1-22 (function map, arg validation, axis handlers)
-# v0.19 - cleanup, code coverage, split out axis parser
-# v0.20 - moved to string types, fixed attribute axis, enabled all zvon tests, started on function tests
-# v0.21 - ???
-
-# TODO
-# move context/input into property of the token
-# change context info (positions/count) into a stack on the context object, for nested predicates
-# implement the missing functions
-# tests for all functions ops
-# check test coverage (a module for this?)
-
 sub new {
 	my $class = shift;
 	my $self = bless {}, $class;
